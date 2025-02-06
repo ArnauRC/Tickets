105a61917a764388e17d6b540d94fb21d26c9e26
ArnauRC <arnau.rc@outlook.es>
2025-02-06 12:54:03 +0100

    relevancia

--- inserts.sql
+++ inserts.sql
@@ -5,15 +5,15 @@ INSERT INTO `tickets`.`tipos` (`tipo_id`, `nombre`) VALUES ('4', 'Duda sobre fun
 INSERT INTO `tickets`.`tipos` (`tipo_id`, `nombre`) VALUES ('5', 'Pedido');
 INSERT INTO `tickets`.`tipos` (`tipo_id`, `nombre`) VALUES ('6', 'Otras tareas');
 
-INSERT INTO `tickets`.`prioridades` (`prioridad_id`, `descripcion`) VALUES ('AL', 'Alta');
-INSERT INTO `tickets`.`prioridades` (`prioridad_id`, `descripcion`) VALUES ('BA', 'Baja');
-INSERT INTO `tickets`.`prioridades` (`prioridad_id`, `descripcion`) VALUES ('ME', 'Media');
-INSERT INTO `tickets`.`prioridades` (`prioridad_id`, `descripcion`) VALUES ('UR', 'Urgente');
+INSERT INTO `tickets`.`prioridades` (`prioridad_id`, `descripcion`, `relevancia`) VALUES ('AL', 'Alta', 3);
+INSERT INTO `tickets`.`prioridades` (`prioridad_id`, `descripcion`, `relevancia`) VALUES ('BA', 'Baja', 1);
+INSERT INTO `tickets`.`prioridades` (`prioridad_id`, `descripcion`, `relevancia`) VALUES ('ME', 'Media', 2);
+INSERT INTO `tickets`.`prioridades` (`prioridad_id`, `descripcion`, `relevancia`) VALUES ('UR', 'Urgente', 4);
 
-INSERT INTO `tickets`.`estados` (`estado_id`, `descripcion`) VALUES ('AB', 'Abierto');
-INSERT INTO `tickets`.`estados` (`estado_id`, `descripcion`) VALUES ('CE', 'Cerrado');
-INSERT INTO `tickets`.`estados` (`estado_id`, `descripcion`) VALUES ('PE', 'Pendiente');
-INSERT INTO `tickets`.`estados` (`estado_id`, `descripcion`) VALUES ('RE', 'Resuelto');
+INSERT INTO `tickets`.`estados` (`estado_id`, `descripcion`, `relevancia`) VALUES ('AB', 'Abierto', 4);
+INSERT INTO `tickets`.`estados` (`estado_id`, `descripcion`, `relevancia`) VALUES ('CE', 'Cerrado', 1);
+INSERT INTO `tickets`.`estados` (`estado_id`, `descripcion`, `relevancia`) VALUES ('PE', 'Pendiente', 3);
+INSERT INTO `tickets`.`estados` (`estado_id`, `descripcion`, `relevancia`) VALUES ('RE', 'Resuelto', 2);
 
 INSERT INTO `tickets`.`empresas` (`nombre`, `cif`) VALUES ('Aúna', 'B21112311');
 INSERT INTO `tickets`.`grupos` (`nombre`) VALUES ('Developer');
@@ -22,8 +22,8 @@ INSERT INTO `tickets`.`agentes` (`nombre`, `correo`, `grupo_id`, `usuario_id`) V
 INSERT INTO `tickets`.`contactos` (`nombre`, `correo`, `telefono1`, `empresa_id`) VALUES ('juan', 'juan@gmail.com', '123123123', 1);
 
 INSERT INTO `tickets`.`tickets` (`descripcion`, `cuerpo`, `propietario`, `agente_id`, `contacto_id`, `estado_id`, `prioridad_id`, `tipo_id`, `fecobj`, `fecfin`) VALUES ('Asunto', 'Descripcion etc etc etc etc etc', 1, 1, 1, 'AB', 'UR', 1, '2025-03-04', '2025-03-05');
-INSERT INTO `tickets`.`tickets` (`descripcion`, `cuerpo`, `propietario`, `agente_id`, `contacto_id`, `estado_id`, `prioridad_id`, `tipo_id`, `fecobj`, `fecfin`) VALUES ('Asunto2', 'Descripcion etc etc etc etc etc', 1, 1, 1, 'AB', 'UR', 5, '2025-03-04', '2025-03-05');
-INSERT INTO `tickets`.`tickets` (`descripcion`, `cuerpo`, `propietario`, `agente_id`, `contacto_id`, `estado_id`, `prioridad_id`, `tipo_id`, `fecobj`, `fecfin`) VALUES ('Prueba de ticket', 'Esto es una prueba de diseño de ticket. Esto es una prueba de diseño de ticket.Esto es una prueba de diseño de ticket.Esto es una prueba de diseño de ticket.', 1, 1, 1, 'AB', 'UR', 6, '2025-03-04', '2025-03-05');
-INSERT INTO `tickets`.`tickets` (`descripcion`, `cuerpo`, `propietario`, `agente_id`, `contacto_id`, `estado_id`, `prioridad_id`, `tipo_id`, `fecobj`, `fecfin`) VALUES ('Asunto', 'Descripcion etc etc etc etc etc', 1, 1, 1, 'AB', 'UR', 2, '2025-03-04', '2025-03-05');
-INSERT INTO `tickets`.`tickets` (`descripcion`, `cuerpo`, `propietario`, `agente_id`, `contacto_id`, `estado_id`, `prioridad_id`, `tipo_id`, `fecobj`, `fecfin`) VALUES ('Asunto2', 'Descripcion etc etc etc etc etc', 1, 1, 1, 'AB', 'UR', 3, '2025-03-04', '2025-03-05');
+INSERT INTO `tickets`.`tickets` (`descripcion`, `cuerpo`, `propietario`, `agente_id`, `contacto_id`, `estado_id`, `prioridad_id`, `tipo_id`, `fecobj`, `fecfin`) VALUES ('Asunto2', 'Descripcion etc etc etc etc etc', 1, 1, 1, 'CE', 'AL', 5, '2025-03-04', '2025-03-05');
+INSERT INTO `tickets`.`tickets` (`descripcion`, `cuerpo`, `propietario`, `agente_id`, `contacto_id`, `estado_id`, `prioridad_id`, `tipo_id`, `fecobj`, `fecfin`) VALUES ('Prueba de ticket', 'Esto es una prueba de diseño de ticket. Esto es una prueba de diseño de ticket.Esto es una prueba de diseño de ticket.Esto es una prueba de diseño de ticket.', 1, 1, 1, 'AB', 'ME', 6, '2025-03-04', '2025-03-05');
+INSERT INTO `tickets`.`tickets` (`descripcion`, `cuerpo`, `propietario`, `agente_id`, `contacto_id`, `estado_id`, `prioridad_id`, `tipo_id`, `fecobj`, `fecfin`) VALUES ('Asunto', 'Descripcion etc etc etc etc etc', 1, 1, 1, 'PE', 'BA', 2, '2025-03-04', '2025-03-05');
+INSERT INTO `tickets`.`tickets` (`descripcion`, `cuerpo`, `propietario`, `agente_id`, `contacto_id`, `estado_id`, `prioridad_id`, `tipo_id`, `fecobj`, `fecfin`) VALUES ('Asunto2', 'Descripcion etc etc etc etc etc', 1, 1, 1, 'RE', 'UR', 3, '2025-03-04', '2025-03-05');
 INSERT INTO `tickets`.`tickets` (`descripcion`, `cuerpo`, `propietario`, `agente_id`, `contacto_id`, `estado_id`, `prioridad_id`, `tipo_id`, `fecobj`, `fecfin`) VALUES ('Prueba de ticket', 'Esto es una prueba de diseño de ticket. Esto es una prueba de diseño de ticket.Esto es una prueba de diseño de ticket.Esto es una prueba de diseño de ticket.', 1, 1, 1, 'AB', 'UR', 4, '2025-03-04', '2025-03-05');
